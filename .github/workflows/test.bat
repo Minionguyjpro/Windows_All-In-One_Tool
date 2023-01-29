ebbde23843436b35265fe3022ffbe1a25f32be0e
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-01-29 10:46:31 +0100

    Update test.bat

--- .github/workflows/test.bat
+++ .github/workflows/test.bat
@@ -1,7 +1,10 @@
 @echo off
 set batch_path=^"D:\a\Windows_All-In-One_Tool\Windows_All-In-One_Tool\src\Windows All-In-One Tool.bat^"
-Starting %batch_patch% for a test...
+echo Starting %batch_patch% for a test...
 "D:\a\Windows_All-In-One_Tool\Windows_All-In-One_Tool\src\Windows All-In-One Tool.bat"
-if errorlevel 0 (
+if errorlevel 1 (
 echo The program ran successfully!
 )
+echo Exiting...
+timeout /t 3 > nul
+exit /b 0
