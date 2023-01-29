4203c656f149d0d37f0172e9a190facadb4fcdfc
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-01-29 10:41:37 +0100

    Create test.bat

---
+++ .github/workflows/test.bat
@@ -0,0 +1,7 @@
+@echo off
+set batch_path=^"D:\a\Windows_All-In-One_Tool\Windows_All-In-One_Tool\src\Windows All-In-One Tool.bat^"
+Starting %batch_patch% for a test...
+"D:\a\Windows_All-In-One_Tool\Windows_All-In-One_Tool\src\Windows All-In-One Tool.bat"
+if errorlevel 0 (
+echo The program ran successfully!
+)
