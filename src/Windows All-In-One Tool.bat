65b8e00ed7362dd8b8a7604ab2a27b4330c7d16a
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-01-31 09:42:38 +0100

    Update Windows All-In-One Tool.bat

--- src/Windows All-In-One Tool.bat
+++ src/Windows All-In-One Tool.bat
@@ -1,4 +1,5 @@
 @echo off
+title Windows All-In-One Tool by Minionguyjpro
 
 :: BatchGotAdmin
 :-------------------------------------
@@ -29,27 +30,31 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------    
 :start
+set back_to_terminal=0
+set back_to_15=0
 cls
 echo Hello there, %username%!
 echo:
-echo                          ^|               Windows All-In-One Tool v1.0.0-beta                     ^|
-echo                          ^|                      Made by Minionguyjpro                            ^|
+echo                          ^|                 Windows All-In-One Tool v1.1.0-stable                 ^|
+echo                          ^|                         Made by Minionguyjpro                         ^|
 echo                          ^|                                                                       ^|
-echo                          ^|[1] DISM and SFC Repair           [5] Check Windows Version            ^|
-echo                          ^|[2] Create Restore Point          [6] Ping Tool                        ^|
-echo                          ^|[3] Windows IP Configuration      [7]                                  ^|
-echo                          ^|[4] User List                     [8] Open Terminal                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
-echo                          ^|[ ]                                                                    ^|
+echo                          ^|[1] DISM and SFC Repair           [21] N/A                             ^|
+echo                          ^|[2] Create Restore Point          [22] N/A                             ^|
+echo                          ^|[3] Windows IP Configuration      [23] N/A                             ^|
+echo                          ^|[4] User List                     [24] N/A                             ^|
+echo                          ^|[5] Check Windows Version         [25] N/A                             ^|
+echo                          ^|[6] Ping Tool                     [26] N/A                             ^|
+echo                          ^|[7] Delete DNS cache              [27] N/A                             ^|
+echo                          ^|[8] Open Terminal                 [28] N/A                             ^|
+echo                          ^|[12] Boot into BIOS/UEFI (WinUEFI)[29] N/A                             ^|
+echo                          ^|[13] Show System Information      [30] N/A                             ^|
+echo                          ^|[14] Show Windows Directory       [31] N/A                             ^|
+echo                          ^|[15] Empty temporary folder       [32] N/A                             ^|
+echo                          ^|[16] N/A                          [33] N/A                             ^|
+echo                          ^|[17] N/A                          [34] N/A                             ^|
+echo                          ^|[18] N/A                          [35] N/A                             ^|
+echo                          ^|[19] N/A                          [36] N/A                             ^|
+echo                          ^|[20] N/A                          [37] N/A                             ^|
 echo                          ^|     [9] Credits          [10] Tool Information          [11] Exit     ^|
 set /p task=Enter a number: 
 if %task%==1 goto 1
@@ -63,6 +68,32 @@ if %task%==8 goto 8
 if %task%==9 goto 9
 if %task%==10 goto 10
 if %task%==11 goto 11
+if %task%==12 goto 12
+if %task%==13 goto 13
+if %task%==14 goto 14
+if %task%==15 goto 15
+if %task%==16 goto 16
+if %task%==17 goto 17
+if %task%==18 goto 18
+if %task%==19 goto 19
+if %task%==20 goto 20
+if %task%==21 goto 21
+if %task%==22 goto 22
+if %task%==23 goto 23
+if %task%==24 goto 24
+if %task%==25 goto 25
+if %task%==26 goto 26
+if %task%==27 goto 27
+if %task%==28 goto 28
+if %task%==29 goto 29
+if %task%==30 goto 30
+if %task%==31 goto 31
+if %task%==32 goto 32
+if %task%==33 goto 33
+if %task%==34 goto 34
+if %task%==35 goto 35
+if %task%==36 goto 36
+if %task%==37 goto 37
 :invalid_task
 cls
 color 47
@@ -84,7 +115,10 @@ echo                               ===  You have entered an invalid number for a
 echo                               ===                                                  ===
 timeout /t 3 > nul
 color 7
-if %back_to_terminal%==0 goto 8
+echo.%back_to_terminal%|findstr /C:"1" >nul 2>&1
+if not errorlevel 1 goto 8
+echo.%back_to_15%|findstr /C:"1" >nul 2>&1
+if not errorlevel 1 goto 15
 goto start
 :1
 cls
@@ -146,22 +180,82 @@ if %another_ping%==Y goto 6
 if %another_ping%==n goto start
 if %another_ping%==N goto start
 <nul set /p "=Done reading? Press a key to proceed..."
-pause >nul
+pause > nul
+:error_ping_detected
+cls
+color 47
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:
+echo                               ===                      ERROR!                      ===
+echo                               ===      Do not add 'ping' to the command. So,       ===
+echo                               === 'ping -t google.com' would become the following: ===
+echo                               === '-t google.com', but that is ONLY for this tool! ===
+timeout /t 8 > nul
+color 7
+goto 6
+:7
+cls
+IPCONFIG.exe /flushdns
+echo:
+echo DNS cache has been deleted!
+timeout /t 3 > nul
+goto start
 :8
 cls
 echo:
 echo:
 echo                          ^|                               Terminals                               ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^| Command Prompt: Command Prompt is included in all Windows systems and ^|
+echo                          ^| is used widely to run many commands to perform certain actions.       ^|
+echo                          ^|                                                                       ^|
+echo                          ^| PowerShell: A console and scripting language which is widely used to  ^|
+echo                          ^| perform many certain actions, on top of some Command Prompt features. ^|
+echo                          ^| This are the default PowerShell version for each Windows OS:          ^|
+echo                          ^|                                                                       ^|
+echo                          ^| v1.0 - Windows XP SP2 and Windows Vista                               ^|
+echo                          ^| v2.0 - Windows 7                                                      ^|
+echo                          ^| v3.0 - Windows 7 SP1 and Windows 8                                    ^|
+echo                          ^| v4.0 - Windows 8.1                                                    ^|
+echo                          ^| v5.0 and v5.1 - Windows 10 / Windows 10 build 14393                   ^|
+echo                          ^| v6.0 - Windows 7/8/10/11, Linux and MacOS                             ^|
+echo                          ^| v7.X.X - Windows 7 (7.2 and lower)/8/10/11, Linux and MacOS           ^|
 echo                          ^|                     Choose a terminal from below:                     ^|
 echo                          ^|                                                                       ^|
-echo                          ^|[1] Command Prompt               [2] Check Windows Version             ^|
+echo                          ^|[1] Command Prompt               [2] Powershell                        ^|
 echo                          ^|                              [3] Go back                              ^|
 set /p terminal_task=Enter a number: 
 if %terminal_task%==1 goto terminal_1
 if %terminal_task%==2 goto terminal_2
 if %terminal_task%==3 goto start
-set back_to_terminal=0
+set back_to_terminal=1
 goto invalid_task
+:terminal_1
+cls
+cmd.exe
+if errorlevel 0 goto 8
+goto start
+:terminal_2
+cls
+if exist "%ProgramFiles%\PowerShell\7\" (
+  pwsh.exe 
+) else (
+  powershell.exe
+)
+if errorlevel 0 goto 8
+if not errorlevel 0 echo PowerShell encountered an error or is not installed!
 :9
 cls
 echo:
@@ -171,6 +265,8 @@ echo                          ^|                         All the credits go to: 
 echo                          ^|                                                                       ^|
 echo                          ^|                  1. Minionguyjpro - Developer and owner               ^|
 echo                          ^| 2. Microsoft - Developer and maker of Windows systems (including CMD) ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
 echo                          ^|                                                                       ^|
 echo                          ^|                                                                       ^|
 echo                          ^|                                                                       ^|
@@ -197,6 +293,23 @@ echo                          ^|                         Tool Information/Info  
 echo                          ^|                                                                       ^|
 echo                          ^|                         Author: Minionguyjpro                         ^|
 echo                          ^|                            Version: v1.0.0                            ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
 echo:
 <nul set /p "=Done reading? Press a key to proceed..."
 pause >nul
@@ -217,33 +330,99 @@ exit
 :11_return
 cls
 goto start
-:terminal_1
-cmd.exe
-:terminal_2
-:terminal_3
+:12
+cls
+echo MAKE SURE TO SAVE ALL YOUR WORK FIRST!
+pause
+cls
+echo Boot into BIOS/UEFI based on WinUEFI (https://github.com/FreakinSoftMania/WinUEFI/)
+timeout /t 3 > nul
+shutdown /fw /f /r /t 00
+:13
+cls
+SYSTEMINFO.exe
+echo:
+<nul set /p "=Done reading/checking? Press a key to proceed..."
+pause >nul
+goto start
+:14
+cls
+echo:
+echo:
+echo                          ^|                     Show (Curent) Windows Directory                   ^|
+echo                          ^| This is the folder where Windows is installed to. It contains all     ^|
+echo                          ^| Windows files for the complete system You may need to check the       ^|
+echo                          ^| current directory for some reason.                                    ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                 Your curent Windows directory is in:                  ^|
+echo                          ^|                              '%windir%'!                            ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo                          ^|                                                                       ^|
+echo:
+<nul set /p "=Done reading/checking? Press a key to proceed..."
+pause >nul
+goto start
+:15
+cls
+set /p force_del_temp=Do you want to force the deletion of all temporary files? This can make currently running programs make run incorrectly. [Y/N] 
+if %force_del_temp%==y goto 15_forced
+if %force_del_temp%==Y goto 15_forced
+if %force_del_temp%==n goto 15_unforced
+if %force_del_temp%==N goto 15_unforced
+set back_to_15=1
+goto invalid_task
+:15_forced
+cls
+del /f "%TMP%\*"
+if %errorlevel%==1 echo Some/No files have been force cleared!
+if %errorlevel%==0 echo The temporary directory has been force cleared!
+<nul set /p "=Done reading/checking the cleared temporary files? Press a key to proceed..."
+pause >nul
+goto start
+:15_unforced
+cls
+del "%TMP%\*"
+if %errorlevel%==1 echo Some/No files have been unforce cleared!
+if %errorlevel%==0 echo The temporary directory has been unforce cleared!
+<nul set /p "=Done reading/checking the cleared temporary files? Press a key to proceed..."
+pause >nul
+goto start
+:16
+:17
+:18
+:19
+:20
+:21
+:22
+:23
+:24
+:25
+:26
+:27
+:28
+:29
+:30
+:31
+:32
+:33
+:34
+:35
+:36
+:37
 :error
 pause
 goto %task%
-:error_ping_detected
-cls
-color 47
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo:
-echo                               ===                      ERROR!                      ===
-echo                               ===      Do not add 'ping' to the command. So,       ===
-echo                               === 'ping -t google.com' would become the following: ===
-echo                               === '-t google.com', but that is ONLY for this tool! ===
-timeout /t 8 > nul
-color 7
-goto 6
